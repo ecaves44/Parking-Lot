8b7b73676640da2f31551e8e53524152f7f1860f
Esau <esaucuevas1994@gmail.com>
2017-08-23 14:24:18 -0500

    Update Vehicle.java

--- Project/Vehicle.java
+++ Project/Vehicle.java
@@ -35,18 +35,3 @@ public abstract class Vehicle {
         return spots;
     }
 }
-//isAvailable is a method in Spot class that has a return statement of 
-    //isAvailable, of true or false
-    //if "spot" is available (true), then whichever spot it is, call the 
-    //type of vehicle (e.g car) to "park"(replace lowercase with Uppercase)
-    //soooo, should my isAvailable method of this Vehicle class be void?
-//    public boolean actualPark(Car c){
-////        if (s.park(this) == true){ //this refers to isAvailable,
-////            c.park(c); //s refers to Spot class --> .Park method which if that
-////        }   //method returns true, call the subclass Car using the c object-ref
-//////        else{ //note: c.void method is ok? void --> sout.
-//////            c.park(c);
-//////        } //getSpotsNeeded?
-////        return c.park(c);
-//    }
-//  
